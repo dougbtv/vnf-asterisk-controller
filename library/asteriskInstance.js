6b1a86abe5a902f09ff539e9abbb6f2d38a5baa2
dougbtv <dosmith@redhat.com>
2017-04-07 14:39:40 -0400

    [increment] has a basic tt-monkeys playback working

--- library/asteriskInstance.js
+++ library/asteriskInstance.js
@@ -1,9 +1,20 @@
 module.exports = function(box_uuid, vac, opts, log) {
 
+  // Our dependencies.
   var client = require('ari-client');
   var async = require("async");
+  var util = require('util');
 
+  // Our public properties.
+
+
+  // Our private properties.
   var initialized = false;
+  var ari = null;
+
+  // ----------------------------------------------
+  // ------- Initialization -----------------------
+  // ----------------------------------------------
 
   // Alrighty, so first thing first...
   // We have to discover this box, and then connect to it.
@@ -11,19 +22,31 @@ module.exports = function(box_uuid, vac, opts, log) {
 
       if (!err) {
 
+        // Connect to ARI
         var url = "http://" + asteriskip + ":" + opts.sourcery_port;
-
-        client.connect(url, opts.ari_user, opts.ari_pass, function (err, ari) {
+        client.connect(url, opts.ari_user, opts.ari_pass, function (err, initial_ari) {
 
           if (!err) {
 
+            // Alright we have an instance created.
             log.it("asteriskinstance_created",{uuid: box_uuid, ip_address: asteriskip});
+
+            // Save the returned ari
+            ari = initial_ari;
+
+            ari.on('StasisStart', stasisStartInbound);
+            ari.on('StasisEnd', stasisEndInbound);
+
+            ari.start('inbound');
 
           } else {
             // This is basically fatal, we discovered it, but couldn't client connect.
             log.error("asteriskinstance_fatalerror_sorceryconnectfailed",{uuid: box_uuid, asteriskip: asteriskip});
           }
         });
+
+        // Wait... should I connect multiple times for multiple apps?
+
 
       } else {
         // This is a fatal error, really.
@@ -32,18 +55,110 @@ module.exports = function(box_uuid, vac, opts, log) {
 
   });
 
+  // ---------------------------------------------- end initialization.
+
+
+  // ---------------------------------------------------
+  // ------------- Applications ------------------------
+  // ---------------------------------------------------
+
+  // ----------------------------------- Stasis Inbound
+  function stasisStartInbound(event, channel) {
+
+    console.log(util.format(
+          'Monkeys! Attack %s!', channel.name));
+
+    var playback = ari.Playback();
+
+    channel.play({media: 'sound:tt-monkeys'}, playback, function(err, newPlayback) {
+      if (err) {
+        log.error("error_asteriskinstance_playbackfail",{err: err});
+        throw err;
+      }
+    });
+
+    playback.on('PlaybackFinished', function(event, completedPlayback) {
+      
+      console.log(util.format(
+          'Monkeys successfully vanquished %s; hanging them up',
+          channel.name));
+
+      channel.hangup(function(err) {
+        if (err) {
+          log.warn("error_asteriskinstance_channelhangup",{note: "looks like an early hangup", err: err});
+        }
+      });
+
+    });
+
+    
+  }
+
+  // handler for StasisEnd event
+  function stasisEndInbound(event, channel) {
+
+    console.log(util.format(
+          'Channel %s just left our application', channel.name));
+  }
+
+  // ----------------------------------- end Stasis Inbound
+
+
   this.originateCall = function(boxid_from,trunk_to,asterisk_app,asterisk_app_data,callback) {
+
+  }
+    /*
+    ari.applications.subscribe({
+      applicationName: 'foobar', 
+      eventSource: val
+      },
+      function (err, application) {
+
+        if (!err) {
+
+          // log.it("trace_originate_call",{ari: ari});
+          var channel = ari.Channel();
+          channel.on('StasisStart', function (event, channel) {
+            log.it("trace_asteriskaction_stasisstart");
+          });
+          channel.on('ChannelDtmfReceived', function (event, channel) {
+            log.it("trace_asteriskaction_dtmf");
+          });
+          channel.originate({
+            endpoint: 'PJSIP/1000@' + trunk_to, 
+            app: 'foobar', 
+            appArgs: 'dialed'
+          },function (err, channel) {
+
+            if (!err) {
+
+              callback(false,"seems...ok");
+
+            } else {
+              log.error("error_asteriskactions_originate",{err: err});
+              callback(err);
+            }
+
+          });
+
+        } else {
+          log.error("error_asteriskactions_subscribe");
+          callback(err);
+        }
+
+      }
+    );
 
     // Why not on instantiation? Because...
     // This may be unique given the box.
 
+        /*
     vac.discoverasterisk.getBoxIP(boxid_from,function(err,asteriskip){
 
       if (!err) {
 
         var url = "http://" + asteriskip + ":" + opts.sourcery_port;
 
-        /*
         async.waterfall([
           function(callback) {
 
@@ -68,52 +183,12 @@ module.exports = function(box_uuid, vac, opts, log) {
           }
 
         });
-        */
 
         client.connect(url, opts.ari_user, opts.ari_pass, function (err, ari) {
 
           if (!err) {
 
-            ari.applications.subscribe({
-              applicationName: 'foobar', 
-              eventSource: val
-              },
-              function (err, application) {
-
-                if (!err) {
-      
-                  // log.it("trace_originate_call",{ari: ari});
-                  var channel = ari.Channel();
-                  channel.on('StasisStart', function (event, channel) {
-                    log.it("trace_asteriskaction_stasisstart");
-                  });
-                  channel.on('ChannelDtmfReceived', function (event, channel) {
-                    log.it("trace_asteriskaction_dtmf");
-                  });
-                  channel.originate({
-                    endpoint: 'PJSIP/1000@' + trunk_to, 
-                    app: 'foobar', 
-                    appArgs: 'dialed'
-                  },function (err, channel) {
-
-                    if (!err) {
-
-                      callback(false,"seems...ok");
-
-                    } else {
-                      log.error("error_asteriskactions_originate",{err: err});
-                      callback(err);
-                    }
-
-                  });
-
-                } else {
-                  log.error("error_asteriskactions_subscribe");
-                  callback(err);
-                }
-
-              }
-            );
+            
   
 
 
@@ -130,9 +205,7 @@ module.exports = function(box_uuid, vac, opts, log) {
       }
 
     });
+        */
 
-
-
-  }
 
 };
