f5ffa00503e2e92d2c14b7c0af9054e1f86f2d54
dougbtv <dosmith@redhat.com>
2017-04-11 17:21:59 -0400

    [originate][increment] apparently assigning a playback & tandem role works, and it can bridge a call on the tandem

--- library/asteriskInstance.js
+++ library/asteriskInstance.js
@@ -122,12 +122,14 @@ module.exports = function(box_uuid, vac, opts, log) {
     switch(inbound_behavior.role) {
       // Playback a sound file.
       case BEHAVIOR_PLAYBACK:
-        behavior_playback(inbound_behavior.destination,event,channel);
+        behavior_playback('sound:' + inbound_behavior.destination,event,channel);
         break;
 
       // Route the call to the next host.
       case BEHAVIOR_TANDEM:
         log.warn("asteriskinstance_behavior_TANDEM_undefined");
+        behavior_tandem('PJSIP/123@' + inbound_behavior.destination,event,channel);
+        break;
 
       // By default... hangup the call and note that it happened in the logs.
       default:
@@ -150,8 +152,140 @@ module.exports = function(box_uuid, vac, opts, log) {
 
   }
 
+  // --------------------------------------------------
   // ---------------------- inbound behaviors
+  // --------------------------------------------------
 
+  // --------------------------------------------------
+  // ---------------------- tandem
+  
+  var behavior_tandem = function (endpoint,event,channel) {
+
+    // TODO: May not keep this playback.
+    /*
+    var playback = ari.Playback();
+    channel.play(
+      {media: 'sound:pls-wait-connect-call'},
+      playback, 
+      function(err, playback) {
+        if (err) {
+          log.error("error_asteriskinstance_originateplaybackerror",{err: err});
+        }
+    });
+    */
+
+    originate_tandem(endpoint,channel);
+
+  }
+
+  function originate_tandem(endpoint,channel) {
+
+    var dialed = ari.Channel();
+
+    channel.on('StasisEnd', function(event, channel) {
+      hangupCalledParty(channel, dialed);
+    });
+
+    dialed.on('ChannelDestroyed', function(event, dialed) {
+      hangupCallingParty(channel, dialed);
+    });
+
+    dialed.on('StasisStart', function(event, dialed) {
+      bridgeChannels(channel, dialed);
+    });
+
+    dialed.originate(
+      {endpoint: endpoint, app: 'bridge-dial', appArgs: 'dialed'},
+      function(err, dialed) {
+        if (err) {
+          log.error("error_asteriskinstance_originatetandem_originate",{err: err});
+        }
+    });
+
+  }
+
+  // handler for original channel hanging up so we can gracefully hangup the
+  // other end
+  function hangupCalledParty(channel, dialed) {
+    // Channelchannel.name left our application, hanging up dialed channel dialed.name
+    log.it("asteriskinstance_hangupcalledparty",{channel_left: channel.name, dailed_channel: dialed.name});
+    
+    // hangup the other end
+    dialed.hangup(function(err) {
+      // ignore error since dialed channel could have hung up, causing the
+      // original channel to exit Stasis
+    });
+  }
+
+  // handler for the dialed channel hanging up so we can gracefully hangup the
+  // other end
+  function hangupCallingParty(channel, dialed) {
+    log.it("asteriskinstance_hangupcallingparty",{hungup_channel: dialed.name, hangingup_channel: channel.name});
+    
+    // hangup the other end
+    channel.hangup(function(err) {
+      // ignore error since original channel could have hung up, causing the
+      // dialed channel to exit Stasis
+    });
+  }
+
+  // handler for dialed channel entering Stasis
+  function bridgeChannels(channel, dialed) {
+    var bridge = ari.Bridge();
+
+    dialed.on('StasisEnd', function(event, dialed) {
+      exitCalledParty(dialed, bridge);
+    });
+
+    dialed.answer(function(err) {
+      if (err) {
+        log.error("error_asteriskinstance_bridgechannels_answer",{err: err});
+      }
+    });
+
+    bridge.create({type: 'mixing'}, function(err, bridge) {
+      if (err) {
+        log.error("error_asteriskinstance_bridgechannels_bridgecreate",{err: err});
+      }
+
+      log.it("asteriskinstance_bridge_create",{"created_bridgeid": bridge.id});
+
+      addChannelsToBridge(channel, dialed, bridge);
+    });
+  }
+
+  // handler for the dialed channel leaving Stasis
+  function exitCalledParty(dialed, bridge) {
+    log.it("asteriskinstance_exitcalledparty",{dialedchannel: dialed.name, destroy_bridge: bridge.id});
+
+    bridge.destroy(function(err) {
+      if (err) {
+        log.error("error_asteriskinstance_dialexit_bridgedestroy",{err: err});
+      }
+    });
+  }
+
+  // handler for new mixing bridge ready for channels to be added to it
+  function addChannelsToBridge(channel, dialed, bridge) {
+    
+    log.it("asteriskinstance_addchannels", {
+      channel_name: channel.name,
+      dialed_name: dialed.name,
+      bridge_id: bridge.id,
+      note: 'Adding channel ' + channel.name + ' (calling party) and dialed channel ' + dialed.name + ' (called party) to bridge ' + bridge.id 
+    });
+
+    bridge.addChannel({channel: [channel.id, dialed.id]}, function(err) {
+      if (err) {
+        log.error("error_asteriskinstance_addchannelstobridge_addchannel",{err: err});
+      }
+    });
+  }
+
+
+  // --------------------------------------------------
+  // ---------------------- playback
+  
   var behavior_playback = function (media,event,channel) {
     
     log.it("asteriskinstance_playbackstarted",{uuid: box_uuid, channel: channel.name, media: media});
